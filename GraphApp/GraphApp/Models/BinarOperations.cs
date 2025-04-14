94f2c7faff230fe568d77f87f9e3d30ed340e497
Akirochka <arkream41@gmail.com>
2025-04-14 09:59:28 +0300

    Binary operations optimization

--- GraphApp/GraphApp/Models/BinarOperations.cs
+++ GraphApp/GraphApp/Models/BinarOperations.cs
@@ -47,6 +47,11 @@ namespace GraphApp.Models
             Name = "Sauvola",
             _operation = (BWV) => Sauvola(BWV),
         },
+        new BinarOperations()
+        {
+            Name = "Wolf",
+            _operation = (BWV) => Wolf(BWV),
+        },
     };
         public static List<BinarOperations> GetBinarOperationsList()
         {
@@ -120,21 +125,133 @@ namespace GraphApp.Models
             IMGView Img = BWV.Img;
             int img_size = Img.Width * Img.Height;
             int stride = Img.Width;
-            byte[] buffer_bytes = (byte[])BWV.ConvertedBytes.Clone();
+            int area = BWV.Area;
+            byte[] buffer_bytes = BWV.ConvertedBytes ;
             byte[] image_bytes = new byte[img_size];
             double t = 0;
-            int pix_i = 0, pix_j = 0;
+            int pix_i = 0, pix_j = 0, count = 0;
+            double avg = 0, avg2 = 0;
+            bool isWolf = param == "Wolf" ? true : false;
+            int min_pix = 255;
+            double max_sigma = 0;
+            if (isWolf)
+            {
+                for (int n = area + stride * area; n < img_size - area - stride * area; n++)
+                {
+                    avg = 0;
+                    avg2 = 0;
+                    count = 0;
+                    for (int i = -area; i <= area; i++)
+                    {
+                        pix_i = n + stride * i;
+                        for (int j = -area; j <= area; j++)
+                        {
+                            pix_j = pix_i + j;
+                            avg += buffer_bytes[pix_j];
+                            avg2 += buffer_bytes[pix_j] * buffer_bytes[pix_j];
+                            ++count;
+                            if (buffer_bytes[pix_j] < min_pix)
+                                min_pix = buffer_bytes[pix_j];
+                        }
+                    }
+                    avg /= count;
+                    avg2 /= count;
+                    double dx = (avg2 - avg * avg);
+                    double sigma = Math.Sqrt(dx);
+                    if (sigma > max_sigma)
+                        max_sigma = sigma;
+
+                }
+
+                for (int n = 0; n < img_size; n++)
+                {
+                    if (n >= area + stride * area && n <= img_size - area - stride * area)
+                        continue;
+                    avg = 0;
+                    avg2 = 0;
+                    count = 0;
+                    for (int i = -area; i <= area; i++)
+                    {
+                        pix_i = n + stride * i;
+                        if (pix_i >= img_size || pix_i < 0)
+                            continue;
+
+                        for (int j = -area; j <= area; j++)
+                        {
+                            pix_j = pix_i + j;
+                            if (pix_j / stride != (pix_j - j) / stride)
+                                continue;
+                            if (pix_j >= img_size || pix_j < 0)
+                                continue;
+                            avg += buffer_bytes[pix_j];
+                            avg2 += buffer_bytes[pix_j] * buffer_bytes[pix_j];
+                            if (buffer_bytes[pix_j] < min_pix)
+                                min_pix = buffer_bytes[pix_j];
+                            ++count;
+                        }
+                    }
+                    avg /= count;
+                    avg2 /= count;
+                    double dx = (avg2 - avg * avg);
+                    double sigma = Math.Sqrt(dx);
+                    if (sigma > max_sigma)
+                        max_sigma = sigma;
+                }
+            }
+
+            for (int n = area + stride * area; n < img_size - area - stride * area; n++)
+            {
+                avg = 0;
+                avg2 = 0;
+                count = 0;
+                for (int i = -area; i <= area; i++)
+                {
+                    pix_i = n + stride * i;
+                    for (int j = -area; j <= area; j++)
+                    {
+                        pix_j = pix_i + j;
+                        avg += buffer_bytes[pix_j];
+                        avg2 += buffer_bytes[pix_j] * buffer_bytes[pix_j];
+                        ++count;
+                    }
+                }
+                avg /= count;
+                avg2 /= count;
+                double dx = (avg2 - avg * avg);
+                double sigma = Math.Sqrt(dx);
+                switch (param)
+                {
+                    case "Niblek":
+                        t = avg + BWV.K * sigma;
+                        break;
+                    case "Sauvola":
+                        t = avg * (1 + BWV.K * (sigma / 128 - 1));
+                        break;
+                    case "Wolf":
+                        t = (1 - BWV.K) * avg + BWV.K * min_pix + BWV.K * (sigma / max_sigma) * (avg - min_pix);
+                        break;
+                }
+
+
+                if (buffer_bytes[n] <= Math.Round(t))
+                    image_bytes[n] = 0;
+                else
+                    image_bytes[n] = 255;
+            }
 
             for (int n = 0; n < img_size; n++) {
-                double avg = 0, avg2 = 0;
-                int count = 0;
-                for (int i = -BWV.Area; i <= BWV.Area; i++)
+                if (n >= area + stride * area && n <= img_size - area - stride * area)
+                    continue;
+                avg = 0;
+                avg2 = 0;
+                count = 0;
+                for (int i = -area; i <= area; i++)
                 {
                     pix_i = n + stride * i;
                     if (pix_i >= img_size || pix_i < 0)
                         continue;
 
-                    for (int j = -BWV.Area; j <= BWV.Area; j++)
+                    for (int j = -area; j <= area; j++)
                     {
                         pix_j = pix_i + j;
                         if (pix_j / stride != (pix_j - j) / stride)
@@ -158,10 +275,13 @@ namespace GraphApp.Models
                     case "Sauvola":
                         t = avg * (1 + BWV.K * (sigma / 128 - 1));
                         break;
+                    case "Wolf":
+                        t = (1 - BWV.K) * avg + BWV.K * min_pix + BWV.K * (sigma / max_sigma) * (avg - min_pix);
+                        break;
                 }
-                
 
-                if (buffer_bytes[n] <= t)
+
+                if (buffer_bytes[n] <= Math.Round(t))
                     image_bytes[n] = 0;
                 else
                     image_bytes[n] = 255;
@@ -172,6 +292,10 @@ namespace GraphApp.Models
         public static byte[] Sauvola(BinarWindowView BWV)
         {
             return Niblek(BWV, "Sauvola");
+        }
+        public static byte[] Wolf(BinarWindowView BWV)
+        {
+            return Niblek(BWV, "Wolf");
         }
         public BinarOperations() { }
     }
